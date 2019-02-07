7e9cff909bed1ddc1668598d158448d5d0db42db
SanthoshBonala <santoshbonala1@gmail.com>
2019-02-07 18:09:32 -0500

    Added Application layout

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularHalModule } from 'angular4-hal'
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExternalService } from './_services/external.service';
+import { ProductComponent } from './product/product.component';
+import { ModuleComponent } from './module/module.component';
+import { ModuleDirective } from './directives/module.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    ModuleComponent,
+    ModuleDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AngularHalModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    { provide: 'ExternalConfigurationService', useClass: ExternalService  }
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
